cd2052eeea1b42bcae6a11abd6d652826536bd3c
Gareth Veitch <garethveitch@gareths-imac.lan>
2021-02-08 16:12:30 -0600

    initial layout commit

--- App.js
+++ App.js
@@ -1,21 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, Image, Dimensions, TouchableOpacity, Modal } from 'react-native';
+import logo from './assets/logo+name.png';
+ 
 export default function App() {
+ 
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Image source={logo} style={styles.logo} />
+      <Text style={styles.slogan}>Keep your communications flowing... and business growing.</Text>
+      <TouchableOpacity
+        onPress={() => setModalVisible(true)}
+        style={styles.button}
+      >
+        <Text>Log In</Text>
+      </TouchableOpacity>
     </View>
   );
 }
-
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+ 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    height: windowHeight,
+    paddingHorizontal: 50,
+    backgroundColor: '#132c41',
     alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: 'space-around',
+  },
+  slogan: {
+    color: "white",
+    fontSize: 32,
+    fontWeight: 'bold',
+  },
+  logo: {
+    width: windowWidth * 0.7,
+    height: windowWidth * 0.145,
+    borderWidth: 2,
+    borderColor: 'green',
+  },
+  button: {
+    backgroundColor: '#f3b64e',
+    borderRadius: 5,
+    paddingVertical: 8,
+    paddingHorizontal: 24,
+    margin: 13,
   },
 });
